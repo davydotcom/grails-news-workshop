e4326eddbb40524001ea235dd14ebdc3ebdd8207
David Estes <davydotcom@gmail.com>
2015-11-17 17:02:20 -0600

    fixing post controller spec

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -2,9 +2,7 @@ class UrlMappings {
 
     static mappings = {
         
-        "/"(controller: 'post', action: 'index')
-        "/create"(controller: 'post', action: 'create')
-        "/save"(controller: 'post', action: 'save')
+        
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
